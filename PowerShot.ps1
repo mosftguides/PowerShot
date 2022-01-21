a01a3f8cd277baebb0a4dca06f116442ba5db50b
mosftguides <95550579+mosftguides@users.noreply.github.com>
2022-01-21 16:27:21 +0100

    Update PowerShot.ps1

--- PowerShot.ps1
+++ PowerShot.ps1
@@ -3,4 +3,4 @@ $Bitmap = new-object System.Drawing.Bitmap 1920,1080
 $Size = New-object System.Drawing.Size 1920,1080
 $FromImage = [System.Drawing.Graphics]::FromImage($Bitmap)
 $FromImage.copyfromscreen(0,0,0,0, $Size,([System.Drawing.CopyPixelOperation]::SourceCopy))
-$Bitmap.Save("$env:Temp\PrintScreen.png",([system.drawing.imaging.imageformat]::png));
+$Bitmap.Save("$env:Temp\PowerShot.png",([system.drawing.imaging.imageformat]::png));
